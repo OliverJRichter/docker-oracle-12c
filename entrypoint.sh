87bf6badf33aa4469b78db3f547fc768398b31d9
OliverJRichter <oliver.richter@adesso.de>
2017-01-27 09:11:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Prevent owner issues on mounted folders
-chown -R oracle:dba /u01/app/oracle
+# chown -R oracle:dba /u01/app/oracle
 rm -f /u01/app/oracle/product
 ln -s /u01/app/oracle-product /u01/app/oracle/product
 
